7b4bc0bcb4ca65a3cf57f850e57998fb6707a948
mhasnain23 <tryfrelancer23@gmail.com>
2024-05-04 11:12:13 -0700

    d

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 // Importing the 'date-fns' library for date manipulation
-import { differenceInSeconds } from 'date-fns';
+import { differenceInSeconds } from "date-fns";
 function getTimeDiffrence(targetDate) {
     const currentDate = new Date();
     return differenceInSeconds(targetDate, currentDate);
 }
+// Function to format seconds into hours, minutes, and seconds
 function formatTime(seconds) {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds / 3600) * 60);
     const remainingSeconds = seconds * 60;
     return `hours ${hours} minutes ${minutes} seconds ${remainingSeconds}`;
 }
-const targetDate = new Date('2024-12-31T23:59:59');
+// Target date for the countdown
+const targetDate = new Date("2024-12-31T23:59:59");
+// Main function to start the countdown
+function startCountdown() {
+    const timeDifference = getTimeDiffrence(targetDate);
+    if (timeDifference > 0) {
+        console.log(`countdown ${formatTime(timeDifference)}`);
+        setTimeout(startCountdown, 1000);
+    }
+    else {
+        console.log('Countdown Ended');
+    }
+}
--- app.ts
+++ app.ts
@@ -1,17 +1,31 @@
 // Importing the 'date-fns' library for date manipulation
-import { differenceInSeconds, format } from 'date-fns';
+import { differenceInSeconds, format } from "date-fns";
 
-function getTimeDiffrence(targetDate:Date){
+function getTimeDiffrence(targetDate: Date) {
     const currentDate = new Date();
-    return differenceInSeconds(targetDate, currentDate)
+    return differenceInSeconds(targetDate, currentDate);
 }
 
-function formatTime(seconds: number):string{
+// Function to format seconds into hours, minutes, and seconds
+function formatTime(seconds: number): string {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds / 3600) * 60);
     const remainingSeconds = seconds * 60;
-    return `hours ${hours} minutes ${minutes} seconds ${remainingSeconds}`
+    return `hours ${hours} minutes ${minutes} seconds ${remainingSeconds}`;
 }
 
+// Target date for the countdown
+const targetDate = new Date("2024-12-31T23:59:59");
 
-const targetDate = new Date('2024-12-31T23:59:59');
+
+// Main function to start the countdown
+function startCountdown() {
+    const timeDifference = getTimeDiffrence(targetDate);
+    if (timeDifference > 0) {
+        console.log(`countdown ${formatTime(timeDifference)}`);
+        setTimeout(startCountdown, 1000)
+    } else {
+        console.log('Countdown Ended');
+
+    }
+}
