2a8e2817e5a686e6d0cb2954f0f5c089de75d180
mhasnain23 <tryfrelancer23@gmail.com>
2024-05-04 10:39:02 -0700

    nit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Importing the 'date-fns' library for date manipulation
+import { differenceInSeconds } from 'date-fns';
+function getTimeDiffrence(targetDate) {
+    const currentDate = new Date();
+    return differenceInSeconds(targetDate, currentDate);
+}
+function formatTime(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds / 3600) * 60);
+    const remainingSeconds = seconds * 60;
+    return `hours ${hours} minutes ${minutes} seconds ${remainingSeconds}`;
+}
---
+++ app.ts
@@ -0,0 +1,15 @@
+// Importing the 'date-fns' library for date manipulation
+import { differenceInSeconds, format } from 'date-fns';
+
+function getTimeDiffrence(targetDate:Date){
+    const currentDate = new Date();
+    return differenceInSeconds(targetDate, currentDate)
+}
+
+function formatTime(seconds: number):string{
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds / 3600) * 60);
+    const remainingSeconds = seconds * 60;
+    return `hours ${hours} minutes ${minutes} seconds ${remainingSeconds}`
+}
+
